a11651f8782f151203e3749cd167f702eb989c3e
leejongseon <92whdtjs@gmail.com>
2023-11-14 13:24:31 +0900

    Initial Commit

---
+++ Earth/EarthApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthApp.swift
+//  Earth
+//
+//  Created by 이종선 on 11/14/23.
+//
+
+import SwiftUI
+
+@main
+struct EarthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
