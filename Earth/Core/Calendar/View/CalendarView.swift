510032ccfa7fe312e455d3d0e3ef6b31d2bb7100
Fine <92whdtjs@gmail.com>
2024-05-29 22:12:07 +0900

    EmptyPostView - View 문구 및 세부 디자인 조정

---
+++ Earth/Core/Calendar/View/CalendarView.swift
@@ -0,0 +1,18 @@
+//
+//  CalendarView.swift
+//  Earth
+//
+//  Created by 이종선 on 5/29/24.
+//
+
+import SwiftUI
+
+struct CalendarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalendarView()
+}
